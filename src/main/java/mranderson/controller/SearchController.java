4d70366d45bfbc6a9726c6e365d9655f475a2abf
Roman <romanveos@gmail.com>
2016-09-27 23:31:03 +0300

    Added searchfield

---
+++ src/main/java/mranderson/controller/SearchController.java
@@ -0,0 +1,31 @@
+package mranderson.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import mranderson.model.Song;
+import mranderson.repository.SongRepository;
+
+@RestController
+@RequestMapping("api/")
+public class SearchController {
+	
+	@Autowired
+	private SongRepository songRepository;
+
+	@GetMapping("search")
+	public ResponseEntity<List<Song>> findByArtist(@PathVariable("artist") String artist) {
+		
+		List<Song> songs = songRepository.findByArtist(artist);
+		
+		return new ResponseEntity<List<Song>>(songs, HttpStatus.OK);
+	}
+}
